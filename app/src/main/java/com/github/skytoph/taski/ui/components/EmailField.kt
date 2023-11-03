dbf92ba06a0ae99d68ecf96d9cc8e2d83df27c0a
toph <toph28555@gmail.com>
2023-11-03 17:33:11 +0200

    create email and password fields

---
+++ app/src/main/java/com/github/skytoph/taski/ui/components/EmailField.kt
@@ -0,0 +1,55 @@
+package com.github.skytoph.taski.ui.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.github.skytoph.taski.R
+
+@Composable
+fun BasicTextField(value: String, errorResId: Int?, onValueChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(text = stringResource(id = R.string.email)) },
+        singleLine = true,
+        maxLines = 1,
+        isError = errorResId != null,
+        keyboardOptions = KeyboardOptions(
+            keyboardType = KeyboardType.Email,
+            imeAction = ImeAction.Done
+        ),
+        supportingText = {
+            Text(
+                text = if (errorResId != null) stringResource(id = errorResId) else "",
+                minLines = 2,
+            )
+        }
+    )
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun TextFieldPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp), contentAlignment = Alignment.Center
+    ) {
+        BasicTextField(
+            value = "value",
+            errorResId = R.string.error_password_should_contain_numeric_character,
+            onValueChange = {}
+        )
+    }
+}
