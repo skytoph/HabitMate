d0ce7df77a26a3e72c6c9cd00c05b2037ff77085
toph <toph28555@gmail.com>
2024-01-11 13:28:49 +0200

    add icon colors object

---
+++ app/src/main/java/com/github/skytoph/taski/presentation/habit/create/IconsColors.kt
@@ -0,0 +1,15 @@
+package com.github.skytoph.taski.presentation.habit.create
+
+import androidx.compose.ui.graphics.Color
+
+object IconsColors {
+    val allColors: List<Color> = listOf(
+        Color.Blue,
+        Color.Red,
+        Color.Yellow,
+        Color.Gray,
+        Color.Green,
+        Color.Magenta,
+        Color.DarkGray
+    )
+}
