583819413e7983f2638c8a8ebf905f9aff989805
toph <toph28555@gmail.com>
2024-01-12 14:41:50 +0200

    add icons colors

--- app/src/main/java/com/github/skytoph/taski/presentation/habit/create/IconsColors.kt
+++ app/src/main/java/com/github/skytoph/taski/presentation/habit/create/IconsColors.kt
@@ -2,14 +2,33 @@ package com.github.skytoph.taski.presentation.habit.create
 
 import androidx.compose.ui.graphics.Color
 
+val BlueBright = Color(0xFF4F4FE5)
+val BlueDark = Color(0xFF285490)
+val BlueLight = Color(0xFF84CACF)
+val GreenDark = Color(0xFF71B067)
+val GreenLight = Color(0xFFBBDE7D)
+val GreenBright = Color(0xFFD3FF82)
+
+val BrownLight = Color(0xFF9C756D)
+val OrangeBright = Color(0xFFF0B459)
+val Yellow = Color(0xFFFFE982)
+val PinkRose = Color(0xFFE6A2D4)
+val PinkPurple = Color(0xFFC382DE)
+val Red = Color(0xFFCD1B4A)
+
 object IconsColors {
     val allColors: List<Color> = listOf(
-        Color.Blue,
-        Color.Red,
-        Color.Yellow,
-        Color.Gray,
-        Color.Green,
-        Color.Magenta,
-        Color.DarkGray
+        BlueBright,
+        BlueDark,
+        BlueLight,
+        GreenDark,
+        GreenLight,
+        GreenBright,
+        Yellow,
+        OrangeBright,
+        PinkRose,
+        PinkPurple,
+        BrownLight,
+        Red,
     )
 }
