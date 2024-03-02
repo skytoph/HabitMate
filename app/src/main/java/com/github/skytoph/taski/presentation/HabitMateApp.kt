94162351feed0b24da118c8b0149e7fc4571efba
toph <toph28555@gmail.com>
2024-03-02 15:45:50 +0200

    separate auth and habit navigation

--- app/src/main/java/com/github/skytoph/taski/presentation/HabitMateApp.kt
+++ app/src/main/java/com/github/skytoph/taski/presentation/HabitMateApp.kt
@@ -1,48 +1,22 @@
 package com.github.skytoph.taski.presentation
 
-import androidx.compose.animation.core.tween
-import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import androidx.navigation.navigation
 import com.github.skytoph.taski.presentation.auth.authentication.AuthViewModel
-import com.github.skytoph.taski.presentation.auth.authentication.AuthenticationScreen
-import com.github.skytoph.taski.presentation.auth.authentication.user.UserData
-import com.github.skytoph.taski.presentation.auth.signin.SignInScreen
-import com.github.skytoph.taski.presentation.auth.signup.SignUpScreen
-import com.github.skytoph.taski.presentation.auth.verify.VerificationScreen
-import com.github.skytoph.taski.presentation.core.nav.ScaleTransitionDirection
-import com.github.skytoph.taski.presentation.core.nav.scaleIntoContainer
-import com.github.skytoph.taski.presentation.core.nav.scaleOutOfContainer
-import com.github.skytoph.taski.presentation.habit.HabitScreens
-import com.github.skytoph.taski.presentation.habit.HabitUi
-import com.github.skytoph.taski.presentation.habit.create.component.CreateHabitScreen
-import com.github.skytoph.taski.presentation.habit.details.components.HabitDetailsScreen
-import com.github.skytoph.taski.presentation.habit.edit.component.EditHabitScreen
-import com.github.skytoph.taski.presentation.habit.icon.component.SelectIconScreen
-import com.github.skytoph.taski.presentation.habit.list.component.HabitsScreen
-import com.github.skytoph.taski.presentation.profile.ProfileScreen
+import com.github.skytoph.taski.presentation.nav.authNavigation
+import com.github.skytoph.taski.presentation.nav.mainNavigation
 
-private abstract class AuthScreens(val route: String) {
-    object Authentication : AuthScreens("authentication")
-    object SignIn : AuthScreens("sign_in")
-    object SignUp : AuthScreens("sign_up")
-    object Verify : AuthScreens("verify")
-
-    companion object {
-        const val route = "auth"
-    }
+object Graph {
+    const val ROOT = "root_graph"
+    const val AUTH = "auth_graph"
+    const val HABITS = "habits_graph"
 }
 
 @Composable
@@ -54,125 +28,13 @@ fun HabitMateApp(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background
     ) {
-        // todo replace with viewModel.currentUser().getStartDestination()
-        val start = HabitScreens.HabitList.route
-
-        NavHost(navController = navController, startDestination = start) {
-            navigation(startDestination = start, route = AuthScreens.route) {
-                composable(route = AuthScreens.Authentication.route) {
-                    AuthenticationScreen(
-                        onSignInClick = { navController.navigate(AuthScreens.SignIn.route) },
-                        onSignUpClick = { navController.navigate(AuthScreens.SignUp.route) },
-                    )
-                }
-                composable(route = AuthScreens.SignIn.route) {
-                    SignInScreen(
-                        onNavigate = {
-                            navController.navigateToProfile(viewModel.currentUser()?.isVerified)
-                        }
-                    )
-                }
-                composable(route = AuthScreens.SignUp.route) {
-                    SignUpScreen(
-                        onNavigate = {
-                            navController.navigateToProfile(viewModel.currentUser()?.isVerified)
-                        })
-                }
-                composable(route = AuthScreens.Verify.route) {
-                    VerificationScreen(
-                        onNavigate = {
-                            navController.navigateAndClear(route = HabitScreens.HabitList.route)
-                        }, navigateUp = {
-                            if (navController.previousBackStackEntry != null) navController.navigateUp()
-                            else navController.navigateAndClear(AuthScreens.Authentication.route)
-                        })
-                }
-                composable(route = HabitScreens.Profile.route) {
-                    ProfileScreen(onSignOut = { navController.signOut(viewModel) })
-                }
-            }
-
-            composable(route = HabitScreens.HabitList.route,
-                exitTransition = { fadeOut(tween(delayMillis = 90)) }) {
-                HabitsScreen(
-                    onCreateHabit = { navController.navigate(HabitScreens.CreateHabit.route) },
-                    onHabitClick = { habit -> navController.navigate(HabitScreens.HabitDetails(habit.id.toString()).route) })
-            }
-            composable(
-                route = HabitScreens.CreateHabit.route,
-                enterTransition = { scaleIntoContainer(direction = ScaleTransitionDirection.OUTWARDS) },
-                exitTransition = { fadeOut(tween(delayMillis = 90)) },
-                popExitTransition = { scaleOutOfContainer(direction = ScaleTransitionDirection.INWARDS) },
-                popEnterTransition = null,
-            ) {
-                CreateHabitScreen(navigateUp = navController::navigateUp,
-                    onSelectIconClick = { navController.navigate(HabitScreens.SelectIcon.route) })
-            }
-            composable(
-                route = HabitScreens.HabitDetails.baseRoute,
-                arguments = listOf(navArgument(name = HabitScreens.HabitDetails.habitIdArg) {
-                    type = NavType.LongType
-                }),
-                enterTransition = { scaleIntoContainer(direction = ScaleTransitionDirection.OUTWARDS) },
-                exitTransition = { fadeOut(tween(delayMillis = 90)) },
-                popExitTransition = { scaleOutOfContainer(direction = ScaleTransitionDirection.INWARDS) },
-                popEnterTransition = null,
-            ) {
-                val habitId = it.arguments?.getLong(HabitScreens.HabitDetails.habitIdArg).toString()
-                HabitDetailsScreen(
-                    navigateUp = navController::navigateUp,
-                    onEditHabit = { navController.navigate(HabitScreens.EditHabit(habitId).route) }
-                )
-            }
-            composable(
-                route = HabitScreens.EditHabit.baseRoute,
-                arguments = listOf(navArgument(name = HabitScreens.EditHabit.habitIdArg) {
-                    type = NavType.LongType
-                    defaultValue = HabitUi.ID_DEFAULT
-                }),
-                enterTransition = { scaleIntoContainer(direction = ScaleTransitionDirection.OUTWARDS) },
-                exitTransition = { fadeOut(tween(delayMillis = 90)) },
-                popExitTransition = { scaleOutOfContainer(direction = ScaleTransitionDirection.INWARDS) },
-                popEnterTransition = null,
-            ) {
-                EditHabitScreen(
-                    navigateUp = navController::navigateUp,
-                    onSelectIconClick = { navController.navigate(HabitScreens.SelectIcon.route) },
-                )
-            }
-            composable(
-                route = HabitScreens.SelectIcon.route,
-                enterTransition = { scaleIntoContainer(direction = ScaleTransitionDirection.OUTWARDS) },
-                popExitTransition = { scaleOutOfContainer(direction = ScaleTransitionDirection.INWARDS) },
-                popEnterTransition = null,
-            ) {
-                SelectIconScreen(navigateUp = navController::navigateUp)
-            }
+        NavHost(
+            navController = navController,
+            route = Graph.ROOT,
+            startDestination = viewModel.startDestination()
+        ) {
+            authNavigation(navController, viewModel)
+            mainNavigation(navController)
         }
     }
-}
-
-private fun NavHostController.signOut(viewModel: AuthViewModel) {
-    viewModel.signOut()
-    navigateAndClear(AuthScreens.Authentication.route)
-}
-
-private fun NavHostController.navigateToProfile(isUserVerified: Boolean?) {
-    if (isUserVerified == true) navigateAndClear(HabitScreens.Profile.route) else navigate(
-        AuthScreens.Verify.route
-    )
-}
-
-private fun NavHostController.navigateAndClear(route: String) {
-    navigate(route = route) {
-        popUpTo(graph.startDestinationId) { inclusive = true }
-    }
-    graph.setStartDestination(route)
-}
-
-private fun UserData?.getStartDestination() = when {
-    this == null -> AuthScreens.Authentication.route
-    this.isVerified == false -> AuthScreens.Verify.route
-    this.isVerified == true -> HabitScreens.HabitList.route
-    else -> AuthScreens.Authentication.route
 }
---
+++ app/src/main/java/com/github/skytoph/taski/presentation/nav/MainNavigation.kt
@@ -0,0 +1,86 @@
+package com.github.skytoph.taski.presentation.nav
+
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeOut
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import androidx.navigation.navigation
+import com.github.skytoph.taski.presentation.Graph
+import com.github.skytoph.taski.presentation.core.nav.ScaleTransitionDirection
+import com.github.skytoph.taski.presentation.core.nav.scaleIntoContainer
+import com.github.skytoph.taski.presentation.core.nav.scaleOutOfContainer
+import com.github.skytoph.taski.presentation.habit.HabitScreens
+import com.github.skytoph.taski.presentation.habit.HabitUi
+import com.github.skytoph.taski.presentation.habit.create.component.CreateHabitScreen
+import com.github.skytoph.taski.presentation.habit.details.components.HabitDetailsScreen
+import com.github.skytoph.taski.presentation.habit.edit.component.EditHabitScreen
+import com.github.skytoph.taski.presentation.habit.icon.component.SelectIconScreen
+import com.github.skytoph.taski.presentation.habit.list.component.HabitsScreen
+
+fun NavGraphBuilder.mainNavigation(navController: NavHostController) {
+    navigation(startDestination = HabitScreens.HabitList.route, route = Graph.HABITS) {
+        composable(route = HabitScreens.HabitList.route,
+            exitTransition = { fadeOut(tween(delayMillis = 90)) }) {
+            HabitsScreen(
+                onCreateHabit = { navController.navigate(HabitScreens.CreateHabit.route) },
+                onHabitClick = { habit -> navController.navigate(HabitScreens.HabitDetails(habit.id.toString()).route) })
+        }
+
+        composable(
+            route = HabitScreens.CreateHabit.route,
+            enterTransition = { scaleIntoContainer(direction = ScaleTransitionDirection.OUTWARDS) },
+            exitTransition = { fadeOut(tween(delayMillis = 90)) },
+            popExitTransition = { scaleOutOfContainer(direction = ScaleTransitionDirection.INWARDS) },
+            popEnterTransition = null,
+        ) {
+            CreateHabitScreen(navigateUp = navController::navigateUp,
+                onSelectIconClick = { navController.navigate(HabitScreens.SelectIcon.route) })
+        }
+
+        composable(
+            route = HabitScreens.HabitDetails.baseRoute,
+            arguments = listOf(navArgument(name = HabitScreens.HabitDetails.habitIdArg) {
+                type = NavType.LongType
+            }),
+            enterTransition = { scaleIntoContainer(direction = ScaleTransitionDirection.OUTWARDS) },
+            exitTransition = { fadeOut(tween(delayMillis = 90)) },
+            popExitTransition = { scaleOutOfContainer(direction = ScaleTransitionDirection.INWARDS) },
+            popEnterTransition = null,
+        ) {
+            val habitId = it.arguments?.getLong(HabitScreens.HabitDetails.habitIdArg).toString()
+            HabitDetailsScreen(
+                navigateUp = navController::navigateUp,
+                onEditHabit = { navController.navigate(HabitScreens.EditHabit(habitId).route) }
+            )
+        }
+
+        composable(
+            route = HabitScreens.EditHabit.baseRoute,
+            arguments = listOf(navArgument(name = HabitScreens.EditHabit.habitIdArg) {
+                type = NavType.LongType
+                defaultValue = HabitUi.ID_DEFAULT
+            }),
+            enterTransition = { scaleIntoContainer(direction = ScaleTransitionDirection.OUTWARDS) },
+            exitTransition = { fadeOut(tween(delayMillis = 90)) },
+            popExitTransition = { scaleOutOfContainer(direction = ScaleTransitionDirection.INWARDS) },
+            popEnterTransition = null,
+        ) {
+            EditHabitScreen(
+                navigateUp = navController::navigateUp,
+                onSelectIconClick = { navController.navigate(HabitScreens.SelectIcon.route) },
+            )
+        }
+
+        composable(
+            route = HabitScreens.SelectIcon.route,
+            enterTransition = { scaleIntoContainer(direction = ScaleTransitionDirection.OUTWARDS) },
+            popExitTransition = { scaleOutOfContainer(direction = ScaleTransitionDirection.INWARDS) },
+            popEnterTransition = null,
+        ) {
+            SelectIconScreen(navigateUp = navController::navigateUp)
+        }
+    }
+}
