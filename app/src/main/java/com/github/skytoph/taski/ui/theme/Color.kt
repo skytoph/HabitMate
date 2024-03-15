f217dbdb70746cf77ba3f89b6c8b7410922ba3ab
toph <toph28555@gmail.com>
2024-03-15 14:05:22 +0200

    fix launcher icon color

--- app/src/main/java/com/github/skytoph/taski/ui/theme/Color.kt
+++ app/src/main/java/com/github/skytoph/taski/ui/theme/Color.kt
@@ -4,7 +4,7 @@ import androidx.compose.ui.graphics.Color
 
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
-val PurpleBright = Color(0xFF6650a4)
+val PurpleBright = Color(0xFF66588D)
 
 val PurpleGrey40 = Color(0xFF625b71)
 
