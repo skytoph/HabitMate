572d5ac9208ec481f5f3e7eb8039b024ceb5b0b8
toph <toph28555@gmail.com>
2024-05-07 18:03:02 +0300

    mark EditableHistoryUi as stable

--- app/src/main/java/com/github/skytoph/taski/presentation/habit/edit/EntryEditableUi.kt
+++ app/src/main/java/com/github/skytoph/taski/presentation/habit/edit/EntryEditableUi.kt
@@ -1,5 +1,6 @@
 package com.github.skytoph.taski.presentation.habit.edit
 
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.text.style.TextAlign
 import com.github.skytoph.taski.presentation.habit.HabitEntryUi
 import com.github.skytoph.taski.presentation.habit.HabitHistoryUi
@@ -8,6 +9,7 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+@Stable
 data class EditableHistoryUi(
     val entries: List<EntryEditableUi> = emptyList(),
     val month: MonthUi
