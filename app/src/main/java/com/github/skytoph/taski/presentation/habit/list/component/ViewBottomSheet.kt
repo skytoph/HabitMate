a2118d374241a78a8e32830c0861c7764b98a7a5
toph <toph28555@gmail.com>
2024-05-18 20:31:26 +0300

    fix reorder button position

--- app/src/main/java/com/github/skytoph/taski/presentation/habit/list/component/ViewBottomSheet.kt
+++ app/src/main/java/com/github/skytoph/taski/presentation/habit/list/component/ViewBottomSheet.kt
@@ -1,18 +1,23 @@
 package com.github.skytoph.taski.presentation.habit.list.component
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.github.skytoph.taski.R
@@ -20,7 +25,6 @@ import com.github.skytoph.taski.presentation.core.component.OptionsDropdown
 import com.github.skytoph.taski.presentation.habit.list.view.FilterOption
 import com.github.skytoph.taski.presentation.habit.list.view.HabitsView
 import com.github.skytoph.taski.presentation.habit.list.view.HabitsViewOptionsProvider
-import com.github.skytoph.taski.presentation.habit.list.view.SortHabits
 import com.github.skytoph.taski.presentation.habit.list.view.SortOption
 import com.github.skytoph.taski.presentation.habit.list.view.ViewOption
 import com.github.skytoph.taski.ui.theme.HabitMateTheme
@@ -35,7 +39,7 @@ fun ViewBottomSheet(
     selectFilter: (FilterOption) -> Unit = {},
     reorder: () -> Unit = {}
 ) {
-    val state = rememberModalBottomSheetState()
+    val state = rememberModalBottomSheetState(skipPartiallyExpanded = true)
     ModalBottomSheet(
         onDismissRequest = hideBottomSheet,
         sheetState = state
@@ -49,27 +53,38 @@ fun ViewBottomSheet(
             )
             Divider()
             OptionsDropdown(
+                title = stringResource(R.string.view_filter),
+                options = HabitsViewOptionsProvider.filterOptions,
+                selected = view.filterBy,
+                selectOption = selectFilter
+            )
+            Divider()
+            OptionsDropdown(
                 title = stringResource(R.string.view_sort),
                 options = HabitsViewOptionsProvider.sortOptions,
                 selected = view.sortBy,
                 selectOption = selectSorting
             )
             Divider()
-            Row {
-                OptionsDropdown(
-                    modifier = Modifier.weight(1f),
-                    title = stringResource(R.string.view_filter),
-                    options = HabitsViewOptionsProvider.filterOptions,
-                    selected = view.filterBy,
-                    selectOption = selectFilter
-                )
-                if (view.sortBy.item == SortHabits.Manually)
-                    Button(onClick = reorder) {
-                        Text(text = "reorder")
-                    }
-            }
+            ReorderButton(reorder)
             Spacer(modifier = Modifier.height(300.dp))
         }
+    }
+}
+
+@Composable
+private fun ReorderButton(reorder: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .padding(horizontal = 8.dp, vertical = 8.dp)
+            .clip(CircleShape)
+            .clickable { reorder() }
+            .padding(horizontal = 8.dp, vertical = 8.dp),
+    ) {
+        Text(
+            text = "reorder",
+            style = MaterialTheme.typography.titleSmall.copy(textDecoration = TextDecoration.Underline)
+        )
     }
 }
 
