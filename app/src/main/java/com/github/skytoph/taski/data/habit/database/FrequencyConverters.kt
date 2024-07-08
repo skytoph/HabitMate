fa32d5859de3b6b3b474a0cbb657faba15b1d4c8
toph <toph28555@gmail.com>
2024-07-08 14:51:59 +0300

    extract sealed class type adapter

--- app/src/main/java/com/github/skytoph/taski/data/habit/database/FrequencyConverters.kt
+++ app/src/main/java/com/github/skytoph/taski/data/habit/database/FrequencyConverters.kt
@@ -1,15 +1,8 @@
 package com.github.skytoph.taski.data.habit.database
 
 import androidx.room.TypeConverter
-import com.google.gson.Gson
+import com.github.skytoph.taski.core.adapter.GeneralTypeAdapterFactory
 import com.google.gson.GsonBuilder
-import com.google.gson.TypeAdapter
-import com.google.gson.TypeAdapterFactory
-import com.google.gson.reflect.TypeToken
-import com.google.gson.stream.JsonReader
-import com.google.gson.stream.JsonWriter
-import kotlin.jvm.internal.Reflection
-import kotlin.reflect.KClass
 
 object FrequencyConverters {
     @TypeConverter
@@ -18,37 +11,5 @@ object FrequencyConverters {
     @TypeConverter
     fun fromFrequency(frequency: FrequencyEntity): String = gson.toJson(frequency, FrequencyEntity::class.java)
 
-    private val gson = GsonBuilder().registerTypeAdapterFactory(
-        object : TypeAdapterFactory {
-            override fun <T : Any> create(gson: Gson, type: TypeToken<T>): TypeAdapter<T> {
-                val classType = Reflection.getOrCreateKotlinClass(type.rawType)
-                return if (classType.sealedSubclasses.any())
-                    SealedClassTypeAdapter(classType, gson)
-                else
-                    gson.getDelegateAdapter(this, type)
-            }
-        }).create()
-}
-
-class SealedClassTypeAdapter<T : Any>(
-    private val classType: KClass<Any>,
-    private val gson: Gson
-) : TypeAdapter<T>() {
-    override fun read(jsonReader: JsonReader): T {
-        jsonReader.beginObject()
-        val nextName = jsonReader.nextName()
-        val innerClass = classType.sealedSubclasses
-            .firstOrNull { it.simpleName!!.contains(nextName) }
-            ?: throw Exception("$nextName is not found among subclasses of the sealed class ${classType.qualifiedName}")
-        val data = gson.fromJson<T>(jsonReader, innerClass.javaObjectType)
-        jsonReader.endObject()
-        return data
-    }
-
-    override fun write(out: JsonWriter, value: T) {
-        val jsonString = gson.toJson(value)
-        out.beginObject()
-        out.name(value.javaClass.canonicalName?.splitToSequence(".")?.last()).jsonValue(jsonString)
-        out.endObject()
-    }
+    private val gson = GsonBuilder().registerTypeAdapterFactory(GeneralTypeAdapterFactory()).create()
 }
